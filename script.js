f28520211fafb4d02454d18b8115de39ed0bd315
Dave Ward <professorx00@gmail.com>
2019-07-25 18:11:17 -0400

    fixed bug of not being able to play gifs and getting multiple versions

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const docGifContainer = $("#gifStorage");
 const docButton = $("#btnSubmit");
 const docformInput = $("#formInput");
 
-
+let counter = 0;
 var gifStorage = ["DUNGEONS AND DRAGONS","MARVEL"];
 
 function createButtons(value){
@@ -24,44 +24,6 @@ function AddToBtns(){
     }
     
 }
-
-function addImagetoGifStorage(object){
-    object.forEach((ele)=>{
-        stillUrl = ele.images.fixed_height_still.url;
-        newImageContainer = $("<div>").addClass("activeImgsContainer").attr("data-rating",ele.rating)
-        newImg = $("<img>").attr("src",stillUrl).attr("data-moving",ele.images.fixed_width.url).addClass("activeImgs").attr("data-active", 0)
-        newSpan = $("<span>").text(`Rating: ${ele.rating}`);
-        newImageContainer.append(newImg,newSpan);
-        docGifContainer.append(newImageContainer);
-        
-    })
-    ImgClick();
-}
-// docformInput.bind('keyup', function(event){ 
-//     if(event.keyCode == 13){ 
-//       event.preventDefault();
-//       return false;
-//     }
-//   });
-
-//intial setting of buttons
-gifStorage.forEach((ele)=>{
-    createButtons(ele);
-})
-// ON click Event lister to add to the div
-docButton.on("click",(event)=>{
-    event.preventDefault();
-    AddToBtns();
-    GifButtonClick();
-})
-
-GifButtonClick();
-
-ImgClick();
-
-
-
-
 function ImgClick() {
     $(".activeImgs").on("click", (event) => {
         img = $(event.target);
@@ -71,12 +33,14 @@ function ImgClick() {
             movingUrl = img.attr("data-moving")
             img.attr("src",movingUrl);
             img.attr("data-moving",stillUrl)
+            img.attr("data-active")==1 
         }
         else{
             stillUrl = img.attr("data-moving");
             movingUrl = img.attr("src")
             img.attr("src",movingUrl);
             img.attr("data-moving",stillUrl)
+            img.attr("data-active")==0
         }
         
     });
@@ -91,8 +55,48 @@ function GifButtonClick() {
             url: queryURL,
             method: "GET"
         }).then(function (response) {
+            console.log(response)
             addImagetoGifStorage(response.data);
         });
     });
 }
+
+function addImagetoGifStorage(object){
+        docGifContainer.empty();
+        counter++
+        console.log("Start Add Images")
+    object.forEach((ele)=>{
+        console.log(counter)
+        stillUrl = ele.images.fixed_height_still.url;
+        newImageContainer = $("<div>").addClass("activeImgsContainer").attr("data-rating",ele.rating)
+        newImg = $("<img>").attr("src",stillUrl).attr("data-moving",ele.images.fixed_width.url).addClass("activeImgs").attr("data-active", 0)
+        newSpan = $("<span>").text(`Rating: ${ele.rating}`);
+        newImageContainer.append(newImg,newSpan);
+        docGifContainer.append(newImageContainer);
+    })
+    console.log("end")
+    ImgClick();
+    response={}
+    console.log("ran ImgClick")
+}
+
+//intial setting of buttons
+gifStorage.forEach((ele)=>{
+    createButtons(ele);
+})
+// ON click Event lister to add to the div
+docButton.on("click",(event)=>{
+    event.preventDefault();
+    AddToBtns();
+    GifButtonClick();
+})
+
+GifButtonClick();
+
+// ImgClick();
+
+
+
+
+
 
